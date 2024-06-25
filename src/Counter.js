c93658675da4aa02837d701a65e7521c51868544
MoizNasir <moiz.99@hotmail.com>
2024-06-25 20:56:57 +0500

    test

--- src/Counter.js
+++ src/Counter.js
@@ -22,7 +22,7 @@ function Counter({ initialCount }) {
         Count: <h3 data-testid="count">{count}</h3>
       </h1>
       <div>
-        <button onClick={increment}> Increment</button>
+        <button onClick={increment}> Increment!</button>
         <button onClick={decrement}> Decrement</button>
         <button onClick={restart}> Restart</button>
         <button onClick={switchSigns}> Switch Signs!!</button>
